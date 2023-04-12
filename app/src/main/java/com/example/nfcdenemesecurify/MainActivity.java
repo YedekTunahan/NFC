061cb656465e58a90c6b89552a40be935fd9ad87
YedekTunahan <elamtraxemon@gmail.com>
2023-04-12 14:25:19 +0300

    İmage taken

--- app/src/main/java/com/example/nfcdenemesecurify/MainActivity.java
+++ app/src/main/java/com/example/nfcdenemesecurify/MainActivity.java
@@ -4,6 +4,7 @@ package com.example.nfcdenemesecurify;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.IsoDep;
@@ -11,6 +12,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,18 +26,31 @@ import org.jmrtd.lds.CardAccessFile;
 import org.jmrtd.lds.DG11File;
 import org.jmrtd.lds.DG1File;
 import org.jmrtd.lds.DG2File;
+import org.jmrtd.lds.FaceImageInfo;
+import org.jmrtd.lds.FaceInfo;
 import org.jmrtd.lds.LDS;
 import org.jmrtd.lds.MRZInfo;
 import org.jmrtd.lds.PACEInfo;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.Security;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    ImageView imageView;
 
+    static {
+        Security.insertProviderAt(new org.spongycastle.jce.provider.BouncyCastleProvider(), 1);
+    }
 
     private View mainLayout;
     private View loadingLayout;
@@ -115,6 +130,7 @@ public class MainActivity extends AppCompatActivity {
             String[][] filter = new String[][]{new String[]{"android.nfc.tech.IsoDep"}};
             adapter.enableForegroundDispatch(this, pendingIntent, null, filter);
         }
+        imageView = findViewById(R.id.photo);
     }
 
     @Override
@@ -136,6 +152,8 @@ public class MainActivity extends AppCompatActivity {
         if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
 
             Tag tag = intent.getExtras().getParcelable(NfcAdapter.EXTRA_TAG);
+            IsoDep nfc = IsoDep.get(tag);
+            nfc.setTimeout(10000);
             Log.e("TAG", String.valueOf(tag));
             if (Arrays.asList(tag.getTechList()).contains("android.nfc.tech.IsoDep")) {
 
@@ -152,7 +170,7 @@ public class MainActivity extends AppCompatActivity {
                 }*/
                 BACKeySpec bacKey = new BACKey("A40U47500","970103","330127");
                 Log.e("Backey", String.valueOf(bacKey));
-                new ReadTask(IsoDep.get(tag), bacKey).execute();
+                new ReadTask(nfc, bacKey).execute();
                 /*mainLayout.setVisibility(View.GONE);
                 loadingLayout.setVisibility(View.VISIBLE);*/
             }
@@ -224,14 +242,13 @@ public class MainActivity extends AppCompatActivity {
         private DG1File dg1File;
         private DG2File dg2File; //MRZ içerisindeki kisisel bilgiler alınır . Ad soyad gibi
         private DG11File dg11File; // Adres ve aytıntılı bilgiler
-        private Bitmap bitmap;
+        public Bitmap bitmap;
 
         @Override
         protected Exception doInBackground(Void... params) {
             try {
 
                 Log.e("doInBackground","doInBackground ÇALIŞTI");
-                isoDep.connect();
 
                 CardService cardService = CardService.getInstance(isoDep);
                 cardService.open();
@@ -260,6 +277,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 Log.e("1","1");
                 service.sendSelectApplet(paceSucceeded);
+                service.doBAC(bacKey);  // Patladığımız yer
                 Log.e("2","2");
                 if (!paceSucceeded) {
                     try {
@@ -277,11 +295,24 @@ public class MainActivity extends AppCompatActivity {
 
                 Log.e("7","7");
                 LDS lds = new LDS();
+                ///////////// DG1FİLE
                 CardFileInputStream dg1In = service.getInputStream(PassportService.EF_DG1);
                 lds.add(PassportService.EF_DG1, dg1In, dg1In.getLength());
                 dg1File = lds.getDG1File();
+
                 Log.e("dg1File", String.valueOf(dg1File));
 
+                ///////////// DG11FİLE
+                CardFileInputStream dg11In = service.getInputStream(PassportService.EF_DG11);
+                lds.add(PassportService.EF_DG11, dg11In, dg11In.getLength());
+                dg11File = lds.getDG11File();
+                ///////////// DG2FİLE
+                CardFileInputStream dg2In = service.getInputStream(PassportService.EF_DG2);
+                lds.add(PassportService.EF_DG2, dg2In, dg2In.getLength());
+                dg2File = lds.getDG2File();
+                Log.e("dg2File", String.valueOf(dg2File));
+
+                bitmap = FaceInfoChangem(dg2File,bitmap);
             } catch (Exception e) {
                 return e;
             }
@@ -296,12 +327,58 @@ public class MainActivity extends AppCompatActivity {
                 MRZInfo mrzInfo = dg1File.getMRZInfo();
 
                 Log.e("Alınan MRZ",mrzInfo.getPrimaryIdentifier());
+                Log.e("DG11","////////////");
+                Log.e("Adress", String.valueOf(dg11File.getPermanentAddress()));
+                Log.e("Other Name", String.valueOf(dg11File.getOtherNames()));
+                Log.e("Personal Number",dg11File.getPersonalNumber());
+                Log.e("Place of Birth", String.valueOf(dg11File.getPlaceOfBirth()));
+                Log.e("Date of Birth (in full)", String.valueOf(dg11File.getFullDateOfBirth()));
+                //Log.e("Telephone Number(s)",dg11File.getTelephone());  //değerlere ulaşılmıyor
+               // Log.e("Profession",dg11File.getProfession());  //değerlere ulaşılmıyor
+                //Log.e("Title",dg11File.getTitle());   //değerlere ulaşılmıyor
+                //Log.e("Personal Summary",dg11File.getPersonalSummary()); //değerlere ulaşılmıyor
+                Log.e("Proof of Citizenship ", String.valueOf(dg11File.getProofOfCitizenship()));
+                Log.e("Number of OtherValid ", String.valueOf(dg11File.getOtherValidTDNumbers()));
+                Log.e("TAG", String.valueOf(dg11File.getTag()));
+               // Log.e("Custody Information",dg11File.getCustodyInformation()); // değerlere ulaşılmıyor
+
+                /// IMAGE
+                imageView.setImageBitmap(bitmap);
 
             } else {
                 //Snackbar.make(passportNumberView, exceptionStack(result), Snackbar.LENGTH_LONG).show();
             }
         }
 
-    }
 
+
+    }
+    public Bitmap  FaceInfoChangem(DG2File dg2File,Bitmap bitmapm){
+    Log.e("ad","adaw");
+
+        List<FaceImageInfo> allFaceImageInfos = new ArrayList<>();
+        List<FaceInfo> faceInfos = dg2File.getFaceInfos();
+        for (FaceInfo faceInfo : faceInfos) {
+            allFaceImageInfos.addAll(faceInfo.getFaceImageInfos());
+        }
+        if (!allFaceImageInfos.isEmpty()) {
+            FaceImageInfo faceImageInfo = allFaceImageInfos.iterator().next();
+
+            int imageLength = faceImageInfo.getImageLength();
+            DataInputStream dataInputStream = new DataInputStream(faceImageInfo.getImageInputStream());
+            byte[] buffer = new byte[imageLength];
+            try {
+                dataInputStream.readFully(buffer, 0, imageLength);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            InputStream inputStream = new ByteArrayInputStream(buffer, 0, imageLength);
+
+            bitmapm = BitmapFactory.decodeStream(inputStream);
+            /*bitmap = ImageUtil.decodeImage(
+                    MainActivity.this, faceImageInfo.getMimeType(), inputStream);*/
+
+        }
+        return bitmapm;
+    }
 }
